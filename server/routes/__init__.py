2e51a9473a6812160727db8377ce5dd24e9d10b8
Ghruank <ghruankkothare@gmail.com>
2025-03-23 00:40:14 +0530

    added chatbot

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -2,3 +2,5 @@ from routes.authRoute import router as auth_router
 from routes.org import router as org
 from routes.repoRoute import router as repo_router
 from routes.allRepos import router as arepo_router
+from routes.scheduleRoute import router as schedule_router
+from routes.chatRoute import router as chat_router
